419c22d168005b26746304e5113a925877904871
최예린 <qsc130@gmail.com>
2022-12-17 02:32:58 +0900

    이항 계쑤 1

--- BOJ/Bronze/11050.py
+++ BOJ/Bronze/11050.py
@@ -19,26 +19,25 @@
 # print(ans)
 #--------------------------
 # 재귀문 사용 #
-import sys
-
-def factorial(n):
-    if n == 0:
-        return 1
-    return n * factorial(n-1)
-
-n, k = map(int, sys.stdin.readline().split())
-print(factorial(n) // (factorial(k) * factorial(n-k)))
-#-----------------------------
-# 반복문 #
-# from sys import stdin
+# import sys
+#
 # def factorial(n):
 #     if n == 0:
 #         return 1
-#     result = 1
-#     for i in range(1, n + 1):
-#         result *= i
-#     return result
+#     return n * factorial(n-1)
 #
-#
-# n, k = map(int, stdin.readline().split())
-# print(factorial(n) // (factorial(k) * factorial(n - k)))
+# n, k = map(int, sys.stdin.readline().split())
+# print(factorial(n) // (factorial(k) * factorial(n-k)))
+#-----------------------------
+# 반복문 #
+from sys import stdin
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+n, k = map(int, stdin.readline().split())
+print(factorial(n) // (factorial(k) * factorial(n - k)))
