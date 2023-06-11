2ce33021644ec552bcf06532bb2682eed9d24707
yerin <qsc130@gmail.com>
2023-06-12 03:33:42 +0900

    0612

--- PRO/level_2/JadenCase_문자열_만들기.js
+++ PRO/level_2/JadenCase_문자열_만들기.js
@@ -11,6 +11,6 @@ function solution(s) {
 //   return s
 //     .toLowerCase()
 //     .split(" ")
-//     .map((e) => e.replace(e[0], e[0].toUpperCase()))
+//     .map((e) => e.replace(e[0], e[0].toUpperCase()))  // e[0]이 없는 경우 -> undefined라서 런타임에서 발생
 //     .join(" ");
 // }
