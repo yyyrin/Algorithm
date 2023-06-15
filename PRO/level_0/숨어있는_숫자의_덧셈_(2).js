92ba05d79e332bf262d695cb865f417d43222734
yerin <qsc130@gmail.com>
2023-06-15 21:52:33 +0900

    0615

--- PRO/level_0/숨어있는_숫자의_덧셈_(2).js
+++ PRO/level_0/숨어있는_숫자의_덧셈_(2).js
@@ -1,4 +1,5 @@
 function solution(my_string) {
+  // 숫자 0~9를 모두 찾는다. (최소 한개)
   const nums = my_string.match(/[0-9]+/g);
   return nums
     ? nums.map((e) => Number(e)).reduce((acc, cur) => acc + cur, 0)
