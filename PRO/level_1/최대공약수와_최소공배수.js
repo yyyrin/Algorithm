33bf944d1d2d23c88b6ec4f67e2b11006e31c21a
yerin <qsc130@gmail.com>
2023-06-18 18:00:24 +0900

    0618

--- PRO/level_1/최대공약수와_최소공배수.js
+++ PRO/level_1/최대공약수와_최소공배수.js
@@ -2,12 +2,14 @@ function solution(n, m) {
   let f_ans = [];
   let ans1 = Math.min(n, m);
   let ans2 = Math.max(n, m);
+  // 최대공약수
   for (let i = ans1; i > 0; i--) {
     if (n % i === 0 && m % i === 0) {
       f_ans.push(i);
       break;
     }
   }
+  // 최소공배수
   for (let i = 1; i <= ans1; i++) {
     if ((i * ans2) % n === 0 && (i * ans2) % m === 0) {
       f_ans.push(i * ans2);
