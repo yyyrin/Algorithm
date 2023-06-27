17293f2bd613734e3e577413bcd4ee18e0ea484c
yerin <qsc130@gmail.com>
2023-06-27 09:50:38 +0900

    0627

--- PRO/level_0/문자열_밀기.js
+++ PRO/level_0/문자열_밀기.js
@@ -4,6 +4,7 @@ function solution(A, B) {
   for (let i = 0; i < A.length; i++) {
     if (compare_A === B) return cnt;
 
+    // 오른쪽으로 한 칸씩 밀고, 마지막 문자는 맨 앞으로 이동
     compare_A = compare_A.substr(-1) + compare_A.slice(0, A.length - 1);
     cnt += 1;
   }
