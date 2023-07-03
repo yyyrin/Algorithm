e56f02794a9fa820dc21b1e2386109f37b592c71
yerin <qsc130@gmail.com>
2023-07-03 10:17:27 +0900

    0703

--- PRO/level_0/겹치는_선분의_길이.js
+++ PRO/level_0/겹치는_선분의_길이.js
@@ -1,4 +1,13 @@
 function solution(lines) {
-  var answer = 0;
-  return answer;
+  let ans = 0;
+  let arr = new Array(200).fill(0); // 배열을 0으로 채우기
+  // 선분이 지나가는 곳마다 +1 해주기(선분이 음수도 있으므로 +100 해주기)
+  lines.forEach((e) => {
+    for (let i = e[0]; i < e[1]; i++) {
+      arr[i + 100] += 1;
+    }
+  });
+  // 1보다 큰 수는 겹치는 부분이므로 count 해주기
+  arr.forEach((e) => (e > 1 ? (ans += 1) : null));
+  return ans;
 }
