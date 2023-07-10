c96a872d590decfd268e9f356459fcf790ce520b
yerin <qsc130@gmail.com>
2023-07-10 16:04:20 +0900

    0710

--- PRO/level_1/문자열_내_마음대로_정렬하기.js
+++ PRO/level_1/문자열_내_마음대로_정렬하기.js
@@ -1,7 +1,9 @@
 function solution(strings, n) {
   return strings.sort((a, b) => {
+    // n번째 문자를 기준으로 오름차순 정렬
     if (a[n] > b[n]) return 1;
     else if (a[n] < b[n]) return -1;
+    // n번째 문자가 같은 경우 사전순으로 정렬
     else return a > b ? 1 : -1;
   });
 }
