3c032e675338f607e3631b80767b026f8f310ee9
yerin <qsc130@gmail.com>
2023-07-14 10:42:34 +0900

    0714

--- PRO/level_2/예상_대진표.js
+++ PRO/level_2/예상_대진표.js
@@ -1,12 +1,10 @@
 function solution(n, a, b) {
-  let a_num = a;
-  let b_num = b;
   let cnt = 0;
 
-  while (a_num !== b_num) {
+  while (a !== b) {
     cnt += 1;
-    a_num = a_num % 2 === 0 ? a_num / 2 : (a_num + 1) / 2;
-    b_num = b_num % 2 === 0 ? b_num / 2 : (b_num + 1) / 2;
+    a = a % 2 === 0 ? a / 2 : (a + 1) / 2;
+    b = b % 2 === 0 ? b / 2 : (b + 1) / 2;
   }
 
   return cnt;
