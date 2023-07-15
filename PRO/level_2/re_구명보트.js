741e7f8d3a073c8a1573902d9e42dd0e8ec85fbb
yerin <qsc130@gmail.com>
2023-07-15 11:41:54 +0900

    0715

---
+++ PRO/level_2/re_구명보트.js
@@ -0,0 +1,11 @@
+// 다시 풀기!
+
+function solution(people, limit) {
+  let ans = 0;
+  people = people.sort((a, b) => b - a);
+
+  for (let i = 0, j = people.length - 1; i <= j; i++, ans++) {
+    if (people[i] + people[j] <= limit) j--;
+  }
+  return ans;
+}
