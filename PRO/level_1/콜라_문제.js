b2f9bcccae834297669e76fa6615ed1956c5ac2f
yerin <qsc130@gmail.com>
2023-07-16 10:32:59 +0900

    0716

--- PRO/level_1/콜라_문제.js
+++ PRO/level_1/콜라_문제.js
@@ -1,11 +1,11 @@
 function solution(a, b, n) {
   let res = 0;
-  let plus;
 
   while (n >= a) {
     let plus = Math.floor(n / a) * b;
     res += plus;
     n = plus + (n % a);
   }
+  
   return res;
 }
