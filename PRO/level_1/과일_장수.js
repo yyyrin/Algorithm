4525c40371096b886064e4176fc80c355817c0b4
yerin <qsc130@gmail.com>
2023-07-20 10:54:20 +0900

    0720

---
+++ PRO/level_1/과일_장수.js
@@ -0,0 +1,13 @@
+function solution(k, m, score) {
+  let ans = 0; // 최대 이익
+  score.sort((a, b) => b - a); // 오름차순으로 정렬
+
+  for (let i = 0; i < score.length; i += m) {
+    // 상자 채워지는 경우
+    if (i + m - 1 < score.length) {
+      ans += score.slice(i, i + m)[m - 1] * m;
+    } else break; // 상자 채워지지 않는 경우
+  }
+
+  return ans;
+}
