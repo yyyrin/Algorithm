56afb160d27d1f3b3370a80b7ad45ce571e175ed
yerin <qsc130@gmail.com>
2023-08-04 16:04:11 +0900

    0804

--- Softeer/근무_시간.js
+++ Softeer/근무_시간.js
@@ -1,22 +1,22 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-let minutes = 0
+let minutes = 0;
 
 // input
 rl.on("line", (input) => {
-    let time = input.split(" ").map(e => {
-        return e.split(":").map(e => Number(e))
-    })
-    minutes += (time[1][0] - time[0][0]) * 60 + time[1][1] - time[0][1];
-})
+  let time = input.split(" ").map((e) => {
+    return e.split(":").map((e) => Number(e));
+  });
+  minutes += (time[1][0] - time[0][0]) * 60 + time[1][1] - time[0][1];
+});
 
 // output
 rl.on("close", () => {
-    console.log(minutes);
-    process.exit();
-})
+  console.log(minutes);
+  process.exit();
+});
