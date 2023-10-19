726e541e594a46ec4ed06da0cb82704a2af1a67f
yeguu <qsc130@gmail.com>
2023-10-19 17:23:19 +0900

    231019_체육복 다시풀기

--- PRO/level_1/체육복.js
+++ PRO/level_1/체육복.js
@@ -23,3 +23,25 @@ function solution(n, lost, reserve) {
 
   return maxStudent - 1;
 }
+
+// 231019 다시 풀기
+function solution(n, lost, reserve) {
+  let student = new Array(n + 1).fill(1);
+  student[0] = 0;
+  reserve.forEach((num) => student[num]++);
+  lost.forEach((num) => student[num]--);
+
+  student.forEach((e, idx) => {
+    if (idx > 0 && e < 1) {
+      if (student[idx - 1] === 2) {
+        student[idx - 1]--;
+        student[idx]++;
+      } else if (student[idx + 1] === 2) {
+        student[idx + 1]--;
+        student[idx]++;
+      }
+    }
+  });
+
+  return student.filter((e) => e > 0).length;
+}
