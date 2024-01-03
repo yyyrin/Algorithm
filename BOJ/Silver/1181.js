d92f5edad2310c8ed56fa39ce54400c4d68dea95
yeguu <qsc130@gmail.com>
2024-01-03 17:56:14 +0900

    240103_단어_정렬

---
+++ BOJ/Silver/1181.js
@@ -0,0 +1,11 @@
+// [BOJ] 1181. 단어 정렬
+
+const fs = require("fs");
+let [N, ...words] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+words = Array.from(new Set(words.map((word) => word.trim("")))); // 중복 제거
+
+words
+  .sort() // 사전 순 정렬
+  .sort((a, b) => a.length - b.length) // 길이 짧은 순 정렬
+  .forEach((word) => console.log(word));
