fae474bdab47410b95a72306e180df7d972bb184
yeguu <qsc130@gmail.com>
2024-03-14 23:59:30 +0900

    240314_풀이_추가

--- PRO/level_0/문자열_뒤집기_2.js
+++ PRO/level_0/문자열_뒤집기_2.js
@@ -4,6 +4,15 @@ function solution(my_string, s, e) {
   for (let i = e; i >= s; i--) {
     result += my_string[i];
   }
-  
+
   return result + my_string.slice(e + 1);
+}
+
+// 풀이 추가
+function solution(my_string, s, e) {
+  return (
+    my_string.slice(0, s) +
+    [...my_string.slice(s, e + 1)].reverse().join("") +
+    my_string.slice(e + 1)
+  );
 }
