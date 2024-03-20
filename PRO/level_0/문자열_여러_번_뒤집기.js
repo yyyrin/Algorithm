c7537e67f353a8d62a38444dd197c826cab15855
yeguu <qsc130@gmail.com>
2024-03-20 17:18:23 +0900

    240320_문자열_여러_번_뒤집기

---
+++ PRO/level_0/문자열_여러_번_뒤집기.js
@@ -0,0 +1,13 @@
+function solution(my_string, queries) {
+  my_string = my_string.split("");
+
+  for (let [s, e] of queries) {
+    my_string = [
+      ...my_string.slice(0, s),
+      ...my_string.slice(s, e + 1).reverse(),
+      ...my_string.slice(e + 1),
+    ];
+  }
+
+  return my_string.join("");
+}
