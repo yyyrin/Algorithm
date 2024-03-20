62a59746d953cf143b2d8ea1aa89be178fa064bb
yeguu <qsc130@gmail.com>
2024-03-20 16:16:15 +0900

    240320_리스트_자르기

---
+++ PRO/level_0/리스트_자르기.js
@@ -0,0 +1,8 @@
+function solution(n, slicer, num_list) {
+  const [a, b, c] = slicer;
+
+  if (n === 1) return num_list.slice(0, b + 1);
+  if (n === 2) return num_list.slice(a);
+  if (n === 3) return num_list.slice(a, b + 1);
+  if (n === 4) return num_list.slice(a, b + 1).filter((_, i) => !(i % c));
+}
