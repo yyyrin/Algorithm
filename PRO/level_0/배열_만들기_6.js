a2d85f87bdfa973b855e3e6bfd6b0e3c2eb5b67c
yeguu <qsc130@gmail.com>
2024-03-24 23:27:46 +0900

    240324_배열_만들기_6

--- PRO/level_0/배열_만들기_6.js
+++ PRO/level_0/배열_만들기_6.js
@@ -1,19 +1,9 @@
 function solution(arr) {
-  let i = 0;
   let stk = [];
-  while (i < arr.length) {
-    if (!stk.length) {
-      stk.push(arr[i]);
-      i++;
-    } else {
-      if (stk[stk.length - 1] === arr[i]) {
-        stk.pop();
-        i++;
-      } else {
-        stk.push(arr[i]);
-        i++;
-      }
-    }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stk.length && stk[stk.length - 1] === arr[i]) stk.pop();
+    else stk.push(arr[i]);
   }
 
   return stk.length ? stk : [-1];
