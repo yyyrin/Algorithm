8731ed39fca5c72c5b92a348a6f5ce9f66c92b7e
yeguu <qsc130@gmail.com>
2024-04-29 01:45:26 +0900

    240429_크레인_인형뽑기_게임

--- PRO/2024/level_1/크레인_인형뽑기_게임.js
+++ PRO/2024/level_1/크레인_인형뽑기_게임.js
@@ -1,33 +1,31 @@
 function solution(board, moves) {
-  let basket = []; // 바구니
-  let doll = 0; // 사라진 인형 개수
+  const obj = {};
+  let stack = [];
+  let cnt = 0;
 
-  for (let i = 0; i < moves.length; i++) {
-    let rowIdx = 0; // 행 idx
-    const colIdx = moves[i] - 1; // 열 idx
-
-    // 해당 열에서 값이 0이 아닌 열 idx 찾기
-    while (rowIdx < board.length && board[rowIdx][colIdx] === 0) {
-      rowIdx++;
+  for (let i = board.length - 1; i >= 0; i--) {
+    for (let j = 0; j < board[0].length; j++) {
+      // board의 해당 값이 0이 아니라면
+      if (board[i][j]) {
+        // 열 인덱스 번호+1을 키 값으로 하는 배열에 해당 값 push
+        obj[j + 1]
+          ? obj[j + 1].push(board[i][j])
+          : (obj[j + 1] = [board[i][j]]);
+      }
     }
-
-    // 행 idx가 board 넘어간 경우
-    if (rowIdx === board.length) continue;
-
-    // 선택된 인형의 모양
-    const selected = board[rowIdx][colIdx];
-
-    // basket의 마지막 인형의 모양과 일치하는 경우 없애기
-    if (basket.length > 0 && Number(basket.slice(-1)) === selected) {
-      basket.pop();
-      doll += 2;
-    } else {
-      // basket의 마지막 인형의 모양과 일치하지 않으면 추가하기
-      basket.push(selected);
-    }
-    // board의 기존 값 0으로 변경
-    board[rowIdx][colIdx] = 0;
   }
 
-  return doll;
+  for (let i = 0; i < moves.length; i++) {
+    // 해당하는 열에 값이 없는 경우
+    if (obj[moves[i]].length < 1) continue;
+
+    const num = obj[moves[i]].pop(); // 크레인이 잡은 인형의 번호
+    // 바구니의 마지막 값과 현재 잡은 번호가 같다면
+    if (stack.length && Number(stack.slice(-1)) === num) {
+      cnt += 2;
+      stack.pop();
+    } else stack.push(num);
+  }
+
+  return cnt;
 }
