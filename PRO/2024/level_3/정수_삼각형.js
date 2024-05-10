d4b03eabc243d6b885fa3b255a97b17875f71852
yeguu <qsc130@gmail.com>
2024-05-10 22:49:29 +0900

    240510_정수_삼각형

---
+++ PRO/2024/level_3/정수_삼각형.js
@@ -0,0 +1,10 @@
+function solution(triangle) {
+  // bottom up 방식으로 최댓값 더해주기
+  for (let i = triangle.length - 1; i > 0; i--) {
+    for (let j = 0; j < triangle[i].length - 1; j++) {
+      triangle[i - 1][j] += Math.max(triangle[i][j], triangle[i][j + 1]);
+    }
+  }
+
+  return triangle[0][0];
+}
